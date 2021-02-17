612a14f6ffc5ec2ccd5c815a7dfb30b921e4b41b
mohssine92 <mohssinelmariouh@gmail.com>
2021-02-17 20:48:52 +0100

    hemos desarolaldo la parte donde un usuario olvida su contraseña , le generamos una nueva y le mandamos por corrreo electronico le avisamos paraque entra a su infor perfil y poner contraseña nueva

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -664,6 +664,253 @@ use PHPMailer\PHPMailer\Exception;   /* =>  para mirar las excepcciones en caso 
 
 	}
 
+	
+	/*=============================================
+	RECUPERAR CONTRASEÑA
+	=============================================*/
+    public function ctrRecuperarPassword(){
+	
+		if(isset($_POST["emailRecuperarPassword"])){
+
+		   if(preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["emailRecuperarPassword"])) {
+			  
+                /*=============================================
+                  GENERAR CONTRASEÑA ALEATORIA
+                =============================================*/
+                
+                function generarPassword($longitud){
+                
+                    $password = "";
+					$patron = "1234567890abcdefghijklmnopqrstuvwxyz";
+					
+					 $max = strlen($patron)-1; 
+                
+                    for($i = 0; $i < $longitud ; $i++){
+                
+					   $password .= substr($patron,rand(0,$max),1);
+					  
+                    }
+                
+                    return $password;
+                
+				}
+				
+				$nuevaPassword = generarPassword(6);  /* => obtendra password generado  */
+
+				$encriptar = crypt($nuevaPassword, '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');    /* => llevar la contraseña aleatoria a la base de datos  */ /* => remplazarla por la que se ha olvidado señor don  */
+
+				$tabla = "usuarios";
+                $item = "email";
+                $valor = $_POST["emailRecuperarPassword"];
+                
+				$traerUsuario = ModeloUsuarios::mdlMostrarUsuario($tabla, $item, $valor);
+				
+				/* var_dump($traerUsuario); */
+
+				if($traerUsuario){
+
+					$id = $traerUsuario["id_u"];
+					$item = "password";
+					$valor = $encriptar;
+					
+					if($traerUsuario['modo'] == 'directo'){
+						
+						$actualizarPassword = ModeloUsuarios::mdlActualizarUsuario($tabla, $id, $item, $valor);  /* => actualizar contraseña en base de datos  */
+
+					}else{
+
+						echo'<script>
+
+						     swal({
+						     		type:"error",
+						     		  title: "¡ERROR!",
+						     		  text: " El correo introducido esta registrado recientemente atraves de redes sociales ",
+						     		  showConfirmButton: true,
+						     		  confirmButtonText: "Cerrar"
+						     	  
+						     }).then(function(result){
+     
+						     		if(result.value){   
+						     			history.back();
+						     		  } 
+						     });
+
+				     	</script>';
+
+
+					}
+					
+
+					if($actualizarPassword  == "ok"){   /* => segnifica actualizo la base de datos correctamente  */
+
+						/*=============================================
+						VERIFICACIÓN CORREO ELECTRÓNICO
+						=============================================*/
+
+						date_default_timezone_set("Africa/Casablanca"); 
+
+						$ruta = ControladorRuta::ctrRuta();
+
+						$mail = new PHPMailer;
+
+						$mail->CharSet = 'UTF-8';
+
+						$mail->isMail();
+
+						$mail->setFrom('mohcineikkou@gmail.com', 'mobmaroc');
+
+						$mail->addReplyTo('mohcineikkou@gmail.com', 'mobmaroc');
+
+						$mail->Subject = "Por favor verifique su dirección de correo electrónico";
+
+						$mail->addAddress($_POST["emailRecuperarPassword"]);
+
+						$mail->msgHTML('<div style="width:100%; background:#eee; position:relative; font-family:sans-serif; padding-bottom:40px">
+	
+							<center>
+								
+								<img style="padding:20px; width:10%" src="https://tutorialesatualcance.com/tienda/logo.png">
+
+							</center>
+
+							<div style="position:relative; margin:auto; width:600px; background:white; padding:20px">
+							
+								<center>
+								
+								<img style="padding:20px; width:15%" src="https://tutorialesatualcance.com/tienda/icon-pass.png">
+
+								<h3 style="font-weight:100; color:#999">SOLICITUD DE NUEVA CONTRASEÑA</h3>
+
+								<hr style="border:1px solid #ccc; width:80%">
+
+								<h4 style="font-weight:100; color:#999; padding:0 20px"><strong>Su nueva contraseña: </strong>'.$nuevaPassword.'</h4>
+
+								<a href="'.$ruta.'" target="_blank" style="text-decoration:none">
+
+								<div style="line-height:30px; background:#0aa; width:60%; padding:20px; color:white">			
+									Haz click aquí
+								</div>
+
+								</a>
+
+								<h4 style="font-weight:100; color:#999; padding:0 20px">Ingrese nuevamente al sitio con esta contraseña y recuerde cambiarla en el panel de perfil de usuario</h4>
+
+								<br>
+
+								<hr style="border:1px solid #ccc; width:80%">
+
+								<h5 style="font-weight:100; color:#999">Si no se inscribió en esta cuenta, puede ignorar este correo electrónico y la cuenta se eliminará.</h5>
+
+								</center>
+
+							</div>
+
+						</div>');
+
+						$envio = $mail->Send();
+
+						if(!$envio){
+
+							echo'<script>
+
+								swal({
+										type:"error",
+									  	title: "¡ERROR!",
+									  	text: "¡Ha ocurrido un problema enviando verificación de correo electrónico a '.$_POST["emailRecuperarPassword"].$mail->ErrorInfo.', por favor inténtelo nuevamente",
+									  	showConfirmButton: true,
+										confirmButtonText: "Cerrar"
+									  
+								}).then(function(result){
+
+										if(result.value){   
+										    history.back();
+										  } 
+								});
+
+							</script>';
+
+						}else{
+
+
+							echo'<script>
+
+								swal({
+									type:"success",
+								  	title: "¡SU SOLICITUD HA SIDO RECIBIDA!",
+								  	text: "¡Por favor revise la bandeja de entrada o la carpeta de SPAM de su correo electrónico '.$_POST["emailRecuperarPassword"].' para su cambio de contraseña!",
+								  	showConfirmButton: true,
+									confirmButtonText: "Cerrar"
+								  
+								}).then(function(result){
+
+										if(result.value){   
+										    history.back();
+										  } 
+								});
+
+							</script>';
+
+						}	
+
+
+					}
+
+
+				}else{
+
+					echo '<script>
+
+						swal({
+							type:"error",
+						  	title: "¡ERROR!",
+						  	text: "¡El correo no existe en el sistema, puede registrase nuevamente con ese correo!",
+						  	showConfirmButton: true,
+							confirmButtonText: "Cerrar"
+						  
+						}).then(function(result){
+
+								if(result.value){   
+								    history.back();
+								  } 
+						});
+
+					</script>';
+
+				}
+
+			    
+			
+		   }else{
+
+		        echo'<script>
+        
+		        	swal({
+		        		type:"error",
+		        		  title: "¡CORREGIR!",
+		        		  text: "¡No se permiten caracteres especiales!",
+		        		  showConfirmButton: true,
+		        		confirmButtonText: "Cerrar"
+		        	  
+		        	}).then(function(result){
+		        
+		        			if(result.value){   
+		        				history.back();
+		        			  } 
+		        	});
+		        
+		        </script>';
+   
+		   }	
+
+
+		}
+
+
+	}
+
+
+	
+
 
 
 
